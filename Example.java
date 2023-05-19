deed2f8e54d53ead6de5565e1a4b28fcf67c2c14
senaleevisal <131803346+senaleevisal@users.noreply.github.com>
2023-05-19 17:39:42 +0530

    Add files via upload

--- Example.java
+++ Example.java
@@ -4,12 +4,51 @@ class Example{
         public static String[] contacts_Name =new String[0];
         public static String[] Contact_Id = new String[1];
         public static String[] contacts_Number =new String[0];
+        public static String[] contact_Company =new String[0];
+        public static int[] contact_Salary =new int[0];
 
         public static void ADD_contacts(){
             Scanner input= new Scanner(System.in);
-
-            name:do{
-                System.out.print("Name          : ");
+            clearConsole();
+            
+            
+            //---------this will print the contact id---------
+            contact_Id:do {
+                int last_num = 0;
+                if(Contact_Id[0]=="0"){
+                    last_num = 0;
+                    String print_num = ""+(last_num+1);
+                    Contact_Id[0] = print_num;
+                    System.out.printf("c%04d",(last_num+1));
+                    System.out.println("\n=====");
+                    break contact_Id;
+                    
+                }else{
+                    last_num = Integer.parseInt(Contact_Id[Contact_Id.length-1]);
+                    
+                    //----------this will coppy the old number-------------
+                    String print_num = ""+(last_num+1);
+                    String[] temp = new String[Contact_Id.length+1];
+                    for(int i=0;i<Contact_Id.length-1;i++){
+                        temp [i] =Contact_Id [i];
+                    }
+                    
+                    //---------this will assign the new number to the Array----
+                    temp[Contact_Id.length] = print_num;
+                    Contact_Id = temp ;
+                    
+                    //---this will print c code
+                    System.out.printf("c%04d",(last_num+1));
+                    System.out.println("\n=====");
+                    break contact_Id;
+                    }
+                } while (true);
+                
+                
+                
+                //---------this will get your name-----------------------
+                name:do{
+                System.out.print("Name           : ");
                 String[] temp=new String[contacts_Name.length+1];
                 for (int i = 0; i < contacts_Name.length; i++){
                     temp[i]=contacts_Name[i];
@@ -19,32 +58,101 @@ class Example{
                 break;
             }while(true);
 
+            
+            
+            //---------this will get your phone number---------------
             phone_num:do{
                 String[] temp=new String[contacts_Number.length+1];
                 for (int i = 0; i < contacts_Number.length; i++){
                     temp[i]=contacts_Number[i];
                 }
-                System.out.print("Number         : ");
-                String input_temp = input.next();
-                if ((input_temp.length()!=10)&&(input_temp.length()!=10)){
-                    System.out.print("\b\b\b\b\b\b\b\b\b\b\b");
-                    continue phone_num;
-                }                
-                temp[temp.length-1]=input_temp;
-                contacts_Number = temp; 
-                break phone_num;
+                ll3:do {
+                    System.out.print("Number         : ");
+                    String input_temp = input.next();
+                    if((input_temp.charAt(0)!=0)&&(input_temp.length()!=10)){
+                        System.out.println("\nInvalid phone number...");
+                        System.out.print("\nDo you want to add phone number again(y/n) ->");
+                        String repeat_option = input.next();
+                        if (repeat_option.equalsIgnoreCase("y")){
+                            System.out.print("\033[5A");
+                            System.out.print("\033[0J");
+                            continue ll3; 
+                        }else{
+                            contacts_Name=delete(contacts_Name);
+                            return;
+                        }
+                    }             
+                    temp[temp.length-1]=input_temp;
+                    contacts_Number = temp; 
+                    break phone_num;
+                    
+                } while (true);
             }while(true);
+
+            
+            
+            
+            
+            //----------------enter cmpany name------------------------
+            do {
+                String[] temp = new String[contact_Company.length+1];
+                for (int i =0; i<contact_Company.length-1;i++){
+                    temp [i] = contact_Company[i];
+                }
+                System.out.print("Company Name  :");
+                temp[contact_Company.length] = input.next();
+                contact_Company=temp;
+                break;
+            } while (true);
+
+
+
+
+
+            //---------------Enter Contact Salary----------------------
+            do {
+                int[] temp = new int[contact_Salary.length+1];
+                for (int i =0; i<contact_Salary.length-1;i++){
+                    temp [i] = contact_Salary[i];
+                }
+                System.out.print("Salary      :");
+                temp[contact_Salary.length] = input.nextInt();
+                contact_Salary = temp;
+                break;
+            } while (true);
+
+
         }
 
-        public static void Check_Num(String input_temp){
-            if((input_temp.charAt(0)==0)&&(input_temp.length()==10)){
-                return;
+
+        public static String[] delete(String[] c){
+            String[] temp =  new String[c.length-1];
+            for(int i=0;i<c.length-2;i++){
+                temp [i] =c [i];
             }
-            System.out.println("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+            c=temp;
+            return contacts_Name;
         }
-        
+
+
+        public final static void clearConsole() {
+            try {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+            new ProcessBuilder("cmd", "/c",
+            "cls").inheritIO().start().waitFor();
+            } else {
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            }
+            } catch (final Exception e) {
+            e.printStackTrace();
+            // Handle any exceptions.
+            }
+            }
         
         public static void main(String[] args) {
+            Contact_Id[0]="0";
             Scanner input =new Scanner(System.in);
             System.out.println("==================================================================================================================");
             System.out.println("\n\n\n");
@@ -65,6 +173,8 @@ class Example{
                 //case 05: LIST_contacts();   break;
                 case 06:                    break;
             }
-            System.out.println(Arrays.toString(contacts_Number));
+            clearConsole();
+            System.out.println(Arrays.toString(contacts_Name));
+            main(args);
     }
 }
